d39701650f667151d89e31208d76aa6245b01bce
Dmitriy <dmitriykharitonov333@gmail.com>
2023-12-04 00:06:40 +0300

    Создал приветствие юзера

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Приветствие юзера
+Console.Write("Введите ваше имя: ");
+#pragma warning disable CS8600
+string username = Console.ReadLine();
+#pragma warning restore CS8600 
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
